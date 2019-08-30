efe1a4f582edc2e274a5b3a26620d653c453f090
yanhw <yanhongwei_a@yahoo.com.sg>
2019-08-31 00:26:50 +0800

    simplest functioning game

--- game/gameBoard.js
+++ game/gameBoard.js
@@ -23,6 +23,101 @@ class GameBoard {
 		// console.log(this.hexagon_list.get(ID_to_key([2,3])))
 		return this.hexagon_list.get(ID_to_key(ID));
 	}
+
+	hasFreeTileToGo(tile_ID, player_ID) {
+		var neighbours = get_hexagon_neighbour_ID(tile_ID);
+		for (var i = 0; i < neighbours.length; i++){
+			var hexagon = this.getHexagon(neighbours[i]);
+			if (hexagon.isEmptyTile()){
+				return true;
+			}
+		}
+		return false;
+	}
+
+	isValidMove(move, player_ID) {
+		var starting_pos = move[0];
+		var ending_pos = move[1];
+		// check if starting pos has player's piece
+		var hexagon = this.getHexagon(starting_pos);
+		var unit = hexagon.getUnit();
+		if (unit === null){
+			return false;
+		}
+		if (unit.getPlayerID() != player_ID) {
+			return false;
+		}
+		// check if ending pos is avaliable
+		var sec_hexagon = this.getHexagon(ending_pos);
+		// console.log(sec_hexagon.is_empty_tile)
+		//!!!!!!!!!!!!!!!!!!!!!
+		if (!sec_hexagon.is_empty_tile) {
+			return false;
+		}
+		return true;
+	}
+	performMovement(move) {
+		var first_ID = move[0];
+		var second_ID = move[1];
+		var first_hexagon =  this.hexagon_list.get(ID_to_key(first_ID));
+		var second_hexagon =  this.hexagon_list.get(ID_to_key(second_ID));
+		var unit = first_hexagon.getUnit();
+		first_hexagon.setUnit(null);
+		second_hexagon.setUnit(unit);
+		unit.setPosition(second_ID);
+	}
+	get_hexagon_neighbours(ID) {
+		var ID_list = get_hexagon_neighbour_ID(ID);
+		var neighbours = [];
+		for (var i = 0; i < ID_list.length; i++) {
+			neighbours.push(this.hexagon_list.get(ID_to_key(ID)));
+		}
+		return neighbours;
+	}
+
+	getNeighbouringEnemies(player_id, position){
+		var neighbours = this.get_hexagon_neighbours(position);
+		var enemy_positions = [];
+		for (var i = 0; i < neighbours.length; i++) {
+			if (neighbours[i].unit != null && neighbours[i].unit.getPlayerID() != player_id){
+				enemy_positions.push(neighbours[i].unit.getPosition());
+			}
+		}
+		return enemy_positions;
+	}
+	isValidActivation(unit, target){
+		if (!unit.free_to_activate || unit.has_activated) {
+			return false;
+		}
+		var target_unit = this.hexagon_list.get(ID_to_key(target)).getUnit();
+		if (unit.getName() === 'delete') {
+			if (target_unit.getPlayerID() === unit.getPlayerID()){
+				return false;
+			}
+			if (target_unit.defeated) {
+				return false;
+			}
+			var neighbours = get_hexagon_neighbour_ID(unit.position);
+			for (var i = 0; i < neighbours.length; i++) {
+				if (neighbours[i] === target_unit.getPosition()){
+					return true;
+				}
+			}
+		}
+		return false;
+	}
+	performAction(unit, target) {
+		if (unit.getNmae() === 'delete') {
+			var target_unit = this.hexagon_list.get(ID_to_key(target)).getUnit();
+			// set unit as activated
+			unit.performAction();
+			// defeat target unit
+			target_unit.defeat();
+			// clean-up gameboard
+			var hexagon = this.hexagon_list.get(ID_to_key(target));
+			hexagon.setUnit(null);
+		}
+	}
 }
 
 
@@ -40,6 +135,7 @@ class Hexagon {
 		return this.neighbour_ID
 	}
 	setAsTile() {
+		// console.log(this.ID)
 		this.is_tile = true;
 		this.is_empty_tile = true;
 	}
@@ -62,6 +158,12 @@ class Hexagon {
 	}
 	isEmptyTile() {
 		return this.is_empty_tile;
+	}
+	isTile() {
+		return this.is_tile;
+	}
+	getID() {
+		return ID;
 	}
 }
 
@@ -101,9 +203,10 @@ function get_hexagon_neighbour_ID(ID){
 
 // [i,j] ==> 'i-j'
 function ID_to_key(ID) {
+	// console.log(ID);
 	var i = ID[0];
 	var j = ID[1];
-	var key = i.toString()+' '+j.toString();
+	var key = i.toString()+'-'+j.toString();
 	return key;
 }
 
@@ -118,3 +221,4 @@ function key_to_ID(key) {
 
 // module.exports.Hexagon = Hexagon;
 module.exports.GameBoard = GameBoard;
+// module.exports. GameBoard.get_hexagon_neighbour_ID =  get_hexagon_neighbour_ID;
---
+++ game/unit.js
@@ -0,0 +1,83 @@
+'use strict';
+
+// super class for all units
+class Unit {
+	constructor(player_id) {
+		this.player_id = player_id;
+		this.has_activated = false;
+		this.free_to_activate = true;
+		this.name = 'unit';
+		this.position = null;
+		this.defeated = false;
+		this.is_mobile = true;
+	}
+
+	setBoardPosition(position) {
+		this.board_position = position;
+	}
+
+	getBoardPosition() {
+		return this.position;
+	}
+	// reset at the end of turn
+	resetActivation() {
+		this.has_activated = false;
+	}
+
+	getName() {
+		return this.name;
+	}
+
+	getPlayerID() {
+		return this.player_id;
+	}
+
+	getPosition() {
+		return this.position;
+	}
+
+	setPosition(position) {
+		this.position = position;
+	}
+
+	getMobileStatus() {
+		return this.is_mobile;
+	}
+
+	defeat() {
+		this.defeated = true;
+		this.is_mobile = false;
+		this.free_to_activate = false;
+		this.position = null;
+	}
+}
+
+
+class Delete extends Unit {
+	constructor(player_id) {
+		super(player_id);
+		this.name = 'delete';
+	}
+
+	can_activate(game_board) {
+		if (!this.free_to_activate) {
+			return false;
+		}
+
+	}
+	delete_any(game_board){
+		var hexagon_list = game_board.get_hexagon_neighbours(this.position);
+		var target = hexagon_list[0].getID()
+		return target;
+
+	}
+	getActivations(game_board){
+		return game_board.getNeighbouringEnemies(this.player_id, this.position);
+	}
+
+	performAction() {
+		this.has_activated = true;
+	}
+}
+
+module.exports.Delete = Delete;
